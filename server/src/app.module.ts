6792ce46e0c822d487640c43a31ac2003fae6e98
Brzozowski7 <krzysztofbrzozowski7@gmail.com>
2023-06-28 13:00:42 +0200

    resigned from hosting FE on BE

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import CustomAppLogger from 'libs/lib/src/logger';
 import { OwnMongooseModule } from 'libs/lib/src/mongo/module';
-
-import { join } from 'path';
 import { ApiModule } from './api/api.module';
 import { AppService } from './app.service';
 import { OwnConfigModule } from './config/config.module';
@@ -11,10 +8,6 @@ import { RoutesModule } from './routes/routes.module';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '../../../client/build'),
-      exclude: ['/api/(.*)'],
-    }),
     CustomAppLogger,
     ApiModule,
     OwnConfigModule,
