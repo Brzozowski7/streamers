e425f715b18ff112a40a188b43db97bd7aa99c0b
Brzozowski7 <krzysztofbrzozowski7@gmail.com>
2023-06-29 18:48:13 +0200

    imagekit instance fix

--- server/libs/lib/src/server/bootstrap/index.ts
+++ server/libs/lib/src/server/bootstrap/index.ts
@@ -48,6 +48,9 @@ export const bootstrapServer = (
       crossOriginEmbedderPolicy: false,
       contentSecurityPolicy: {
         useDefaults: true,
+        directives: {
+          'img-src': ["'self'", 'https: data:'],
+        },
       },
     }),
   );
