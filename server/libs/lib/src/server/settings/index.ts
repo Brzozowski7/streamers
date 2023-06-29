1a02d32e88b2cce78eac26a994752b91de342821
Brzozowski7 <krzysztofbrzozowski7@gmail.com>
2023-06-29 17:39:43 +0200

    ssl cert config

--- server/libs/lib/src/server/settings/index.ts
+++ server/libs/lib/src/server/settings/index.ts
@@ -1,10 +1,23 @@
 import { NestApplicationOptions } from '@nestjs/common';
 import dotenv from 'dotenv';
+import fs from "fs"
 dotenv.config();
 
 const COMMON_SERVER_SETTINGS: NestApplicationOptions = {
   bufferLogs: true,
   cors: true,
 };
+
+if (process.env.NODE_ENV === 'production') {
+  const certsRootPath = `/etc/letsencrypt/live/${process.env.APP_DOMAIN}/`;
+  const key = fs.readFileSync(certsRootPath + 'privkey.pem');
+  const cert = fs.readFileSync(certsRootPath + 'cert.pem');
+  const ca = fs.readFileSync(certsRootPath + 'chain.pem');
+  COMMON_SERVER_SETTINGS['httpsOptions'] = {
+    key,
+    cert,
+    ca,
+  };
+}
 
 export default COMMON_SERVER_SETTINGS;
